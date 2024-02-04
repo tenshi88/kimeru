4917e836ee92b476c770aa38947eb81c9a347035
tenshi88 <144685875+tenshi88@users.noreply.github.com>
2024-02-04 09:31:05 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,18 +1,17 @@
-    const result = document.getElementById("result");
-    const btn = document.getElementById("btn");
+const result = document.getElementById("result");
+const btn = document.getElementById("btn");
 
-    btn.addEventListener("click", () => {
-        const n = Math.floor(Math.random()*3);
-        switch(n){
-            case 0:
-                result.textContent = "やるっ!! (^o^)";
-                break;
-            case 1:
-                result.textContent = "やらない(-_-)";
-                break;
-            case 2:
-                result.textContent = "明日考えよ(^^)♪"
-        }
-    })
-
-
+btn.addEventListener("click", () => {
+    const r = Math.random();
+    const resultImage = document.getElementById('resultImage');
+    if (r < 0.5) {
+        result.textContent = "やるっ!! (^o^)";
+        resultImage.src = './images/go_sign.jpg'; 
+    } else if (r < 0.75) {
+        result.textContent = "やらない(-_-)";
+        resultImage.src ='./images/stop_sign.jpg'; 
+    } else {
+        result.textContent = "明日考えよ(^^)♪";
+        resultImage.src ='./images/smile_sign.jpg'; 
+    }
+});
