d429fe2309b93e45b24593f98a97f7f7b008f168
tenshi88 <144685875+tenshi88@users.noreply.github.com>
2024-02-04 10:23:55 +0900

    Update main.js

--- main.js
+++ main.js
@@ -6,12 +6,12 @@ btn.addEventListener("click", () => {
     const resultImage = document.getElementById('resultImage');
     if (r < 0.5) {
         result.textContent = "やるっ!! (^o^)";
-        resultImage.src = '/images/go_sign.jpg'; 
+        resultImage.src = 'go_sign.jpg'; 
     } else if (r < 0.75) {
         result.textContent = "やらない(-_-)";
-        resultImage.src ='/images/stop_sign.jpg'; 
+        resultImage.src ='stop_sign.jpg'; 
     } else {
         result.textContent = "明日考えよ(^^)♪";
-        resultImage.src ='/images/smile_sign.jpg'; 
+        resultImage.src ='smile_sign.jpg'; 
     }
 });
